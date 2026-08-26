c363f26f43307a7e5a1ae2375190711e3f1c8aac
Caua11296 <caua.hernandes@edu.senai.br>
2026-08-26 10:38:36 -0300

    altecao1

---
+++ funcoes.php
@@ -0,0 +1,11 @@
+<?php
+function verificar($usuario, $senha){
+    if ($usuario === "admin" && $senha === "1234"){
+        header("Location: painel.php");
+        exit;
+    } else {
+        $msg = urlencode("Usuário ou senha inválidos!");
+        header("Location: index.php?msg= $msg");
+        exit;
+    }
+} 
